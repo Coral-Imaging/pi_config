13e8355134924f9fa75aa84d7bd580b9f51be0ae
Dorian Tsai <dorian.tsai@gmail.com>
2022-10-27 19:31:49 +1000

    git pull for all pis and git settings

--- pi_file_sync.sh
+++ pi_file_sync.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 # assume /etc/hosts file is setup with each Pi, named and numbered
 # ssh cslics04@cslics03 mkdir /home/cslics04/temp_ws 2>&1 | tee cslics03.log
@@ -9,15 +9,50 @@
 # ssh cslics04@cslics03 mkdir /home/cslics04/temp_ws 2>&1 | tee cslics03.log
 
 
+
 # generate ssh key on main computer (similar to github key-gen)
 # copy over id key to each pi
 # ssh-copy-id -i ~/.ssh/id_ed25519.pub cslics04@cslics03
 
 
-echo "cslics01: cloning repos"
-ssh cslics04@cslics01 rm -rf temp_ws 2>&1 | tee cslics01.log
-ssh cslics04@cslics01 git clone git@github.com:Coral-Imaging/coral_spawn_imager.git /home/cslics04/temp_ws/coral_spawn_imager; ssh cslics04@cslics01 git clone git@github.com:Coral-Imaging/rrap-scheduler.git /home/cslics04/temp_ws/rrap-scheduler; ssh cslics04@cslics01 git clone git@github.com:Coral-Imaging/rrap-server.git /home/cslics04/temp_ws/rrap-server 2>&1 | tee cslics01.log
+echo "cslics01: pulling repos"
+# ssh cslics04@cslics01 rm -rf temp_ws 2>&1 | tee cslics01.log
+# ssh cslics04@cslics01 git clone git@github.com:Coral-Imaging/coral_spawn_imager.git /home/cslics04/temp_ws/coral_spawn_imager; ssh cslics04@cslics01 git clone git@github.com:Coral-Imaging/rrap-scheduler.git /home/cslics04/temp_ws/rrap-scheduler; ssh cslics04@cslics01 git clone git@github.com:Coral-Imaging/rrap-server.git /home/cslics04/temp_ws/rrap-server 2>&1 | tee cslics01.log
 
-echo "cslics03: cloning repos"
-ssh cslics04@cslics03 rm -rf temp_ws 2>&1 | tee cslics03.log
-ssh cslics04@cslics03 git clone git@github.com:Coral-Imaging/coral_spawn_imager.git /home/cslics04/temp_ws/coral_spawn_imager; ssh cslics04@cslics03 git clone git@github.com:Coral-Imaging/rrap-scheduler.git /home/cslics04/temp_ws/rrap-scheduler; ssh cslics04@cslics03 git clone git@github.com:Coral-Imaging/rrap-server.git /home/cslics04/temp_ws/rrap-server 2>&1 | tee cslics03.log
+# scp qcr-env.bash cslics04@cslics01:/home/cslics04/qcr-env.bash
+# ssh cslics04@cslics01 sudo mv /home/cslics04/qcr-env.bash /etc/qcr/qcr-env.bash 2>&1 | tee cslics01.log # I ended up just loging in and `sudo mv`
+
+ssh cslics04@cslics01 git config --global pull.rebase true 2>&1 | tee cslics01.log
+
+ssh cslics04@cslics01 git -C /home/cslics04/cslics_ws/src/coral_spawn_imager pull git@github.com:Coral-Imaging/coral_spawn_imager.git 2>&1 | tee cslics01.log
+
+ssh cslics04@cslics01 git -C /home/cslics04/cslics_ws/src/rrap-server pull git@github.com:Coral-Imaging/rrap-server.git 2>&1 | tee cslics01.log
+
+ssh cslics04@cslics01 git -C /home/cslics04/cslics_ws/src/rrap-scheduler pull git@github.com:Coral-Imaging/rrap-scheduler.git 2>&1 | tee cslics01.log
+
+
+
+echo "cslics03: pulling repos"
+
+# ssh cslics04@cslics03 rm -rf temp_ws 2>&1 | tee cslics03.log
+ssh cslics04@cslics01 git config --global pull.rebase true
+
+ssh cslics04@cslics03 git -C /home/cslics04/cslics_ws/src/coral_spawn_imager pull git@github.com:Coral-Imaging/coral_spawn_imager.git 2>&1 | tee cslics03.log
+
+ssh cslics04@cslics03 git -C /home/cslics04/cslics_ws/src/rrap-server pull git@github.com:Coral-Imaging/rrap-server.git 2>&1 | tee cslics03.log
+
+ssh cslics04@cslics03 git -C /home/cslics04/cslics_ws/src/rrap-scheduler pull git@github.com:Coral-Imaging/rrap-scheduler.git 2>&1 | tee cslics03.log
+
+
+# ssh cslics04@cslics03 git clone git@github.com:Coral-Imaging/rrap-scheduler.git /home/cslics04/temp_ws/rrap-scheduler; ssh cslics04@cslics03 git clone git@github.com:Coral-Imaging/rrap-server.git /home/cslics04/temp_ws/rrap-server 2>&1 | tee cslics03.log
+
+echo "cslics04: pulling repos"
+
+# ssh cslics04@cslics03 rm -rf temp_ws 2>&1 | tee cslics03.log
+ssh cslics04@cslics04 git config --global pull.rebase true
+
+ssh cslics04@cslics04 git -C /home/cslics04/cslics_ws/src/coral_spawn_imager pull git@github.com:Coral-Imaging/coral_spawn_imager.git 2>&1 | tee cslics04.log
+
+ssh cslics04@cslics04 git -C /home/cslics04/cslics_ws/src/rrap-server pull git@github.com:Coral-Imaging/rrap-server.git 2>&1 | tee cslics04.log
+
+ssh cslics04@cslics04 git -C /home/cslics04/cslics_ws/src/rrap-scheduler pull git@github.com:Coral-Imaging/rrap-scheduler.git 2>&1 | tee cslics04.log
---
+++ start_imaging.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# start camera trigger on all pi's
+
+# assume hostnames setup on all pi's in /etc/hosts file
+
