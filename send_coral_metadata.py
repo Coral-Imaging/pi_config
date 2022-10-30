bfeb40ca9c7e139052eae6d3e6c331b1c81b5806
Dorian Tsai <dorian.tsai@gmail.com>
2022-10-30 21:17:03 +1000

    send coral metadata script to pi over rrap-server

---
+++ send_coral_metadata.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+# send coral metadata
+
+import os
+import json
+import requests
+
+CSLICS_ID = 'cslics03'
+REMOTE = f'{CSLICS_ID}:5000'
+
+# read json as dictionary
+CORAL_METADATA_FILE = 'coral_metadata.json'
+with open(CORAL_METADATA_FILE) as f:
+    sendmetadata = json.load(f)
+
+# send dictionary
+resp = requests.post(f'http://{REMOTE}/set-coral-metadata', json=sendmetadata)
+
+print(resp)
